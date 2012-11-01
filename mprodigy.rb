de3a0fd0bf7feaf55b79d5e7652ff7b0d4be7150
roylines <roy@roylines.co.uk>
2012-11-01 13:01:01 +0000

    added monkey patch for SyncEngine login/logout

--- mprodigy.rb
+++ mprodigy.rb
@@ -2,6 +2,10 @@ module Mprodigy
 
 class API
 
+  def self.init() {
+    # just here for monkey patching
+  }
+
   def self.sessionBegin(applicationId, version, instance, other)
      return Mprodigy::native_sessionBegin(applicationId, version, instance, other)
   end 
@@ -22,3 +26,19 @@ end
 
 end
 
+module SyncEngine
+  alias self.oldlogin self.login
+
+  alias self.oldlogout self.logout
+  
+  def self.login(login, password, callback_url)
+    RhoLog::info('Mprodigy::SyncEngine::login', login)
+    self.oldlogin(login, password, callback_url)
+  end
+
+  def self.logout()
+    RhoLog::info('Mprodigy::SyncEngine::logout', logout)
+    self.oldlogout()
+  end
+
+end
